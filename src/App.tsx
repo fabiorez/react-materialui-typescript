52c84b31751ed1c703723aecdb2fd7a6e8a0a705
fabio <fabio.rezende@gmail.com>
2023-04-25 17:35:52 -0300

    feat: adicionado responsividade no menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import { AppThemeProvider } from './shared/contexts';
+import { AppDrawerProvider, AppThemeProvider } from './shared/contexts';
 import { AppRoutes } from './routes';
 import { MenuLateral } from './shared/components';
 
 export const App = () => {
   return (
     <AppThemeProvider>
-      <BrowserRouter>
-        <MenuLateral>
-          <AppRoutes />
-        </MenuLateral>
-      </BrowserRouter>
+      <AppDrawerProvider>
+        <BrowserRouter>
+
+          <MenuLateral>
+            <AppRoutes />
+          </MenuLateral>
+        
+        </BrowserRouter>
+      </AppDrawerProvider>
     </AppThemeProvider>
   );
 };
